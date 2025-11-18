13a5d4afaa2b520e6fcd698d4f63a84216ed62b6
Andrey2025-lang <andrey.gonzalez.villegas@est.una.ac.cr>
2025-11-17 23:19:58 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("openBtn").addEventListener("click", function () {
+  document.getElementById("envelope").style.display = "none";
+  this.style.display = "none";
+  document.getElementById("mensaje").style.display = "block";
+});
